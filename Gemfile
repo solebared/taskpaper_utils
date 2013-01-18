a064e52fa5782eab10f7043d1febf6acdc0305e8
exbinary <exbinary@gmail.com>
2013-01-19 01:05:07 +0530

    Initial commit based on bundle gem

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in taskpaper-parser.gemspec
+gemspec
---
+++ lib/taskpaper-parser.rb
@@ -0,0 +1,7 @@
+require "taskpaper-parser/version"
+
+module Taskpaper
+  module Parser
+    # Your code goes here...
+  end
+end
