68cdff4aba33ca4b480785b85ea5366badcd3d5e
exbinary <exbinary@gmail.com>
2013-11-21 17:55:23 -0500

    Move exemplar.taskpaper directly into spec/integration dir

--- spec/integration/parse_and_serialize_integration_spec.rb
+++ spec/integration/parse_and_serialize_integration_spec.rb
@@ -6,15 +6,17 @@ module TaskpaperTools
 
     describe 'parsing and immediately saving the parsed object graph' do
 
+      let(:exemplar_path) { File.join(__dir__, 'exemplar.taskpaper') }
+
       it 'results in a new file identical to the original' do
         # since the new file is based on the object graph, this serves as a good
         # integration test - anything we either don't parse or serialize correctly
         # will result in differing files
-        document = TaskpaperTools.parse('spec/fixtures/exemplar.taskpaper')
+        document = TaskpaperTools.parse(exemplar_path)
         Dir::Tmpname.create('taskpaper_tools_integration_spec-') do |new_file|
           begin
             TaskpaperTools.save(document, new_file)
-            expect(File.read('spec/fixtures/exemplar.taskpaper')).to eql(File.read(new_file))
+            expect(File.read(exemplar_path)).to eql(File.read(new_file))
           ensure
             File.delete(new_file) if File.exist?(new_file)
           end
